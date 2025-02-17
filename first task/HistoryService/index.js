27c472db8ae8c26f4ac979fd2b2e59078f7daee4
kalabass <tid.21@uni-dubna.ru>
2025-02-17 16:57:46 +0300

    11

--- first task/HistoryService/index.js
+++ first task/HistoryService/index.js
@@ -1,16 +1,28 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
+import { fileURLToPath } from 'url'; // Импортируйте fileURLToPath
 import { historyRouter } from './src/routers/history.router.js';
 import { historyService } from './src/services/history.service.js';
 import swaggerSpecs from './swagger-config.js';
+
 dotenv.config();
 const app = express();
 
+// Получите путь к текущему модулю
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
-app.use(express.json());
+const manifestPath = path.join(__dirname, 'manifest.json');
 
+app.get('/manifest.json', (req, res) => {
+  res.sendFile(manifestPath);
+});
+
+app.use(express.json());
 app.use(historyRouter);
 
 const port = process.env.PORT || 5253;
